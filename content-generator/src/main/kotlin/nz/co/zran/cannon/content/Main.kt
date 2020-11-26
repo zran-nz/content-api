2bd1b7163ae9f1463f08f85929a0ecc866005cc6
Leon Zhou <zhouleib1412@gmail.com>
2020-11-26 19:09:31 +1300

    update generator

--- content-generator/src/main/kotlin/nz/co/zran/cannon/content/Main.kt
+++ content-generator/src/main/kotlin/nz/co/zran/cannon/content/Main.kt
@@ -1,5 +1,18 @@
-package com.github.b1412.generator
+package nz.co.zran.cannon.content
+
+import com.github.b1412.generator.client.HttpJsonGenerator
+import com.github.b1412.generator.ktGenerator
 
 fun main() {
-    ktGenerator("generator.properties")
+    ktGenerator(
+            config = "generator.properties",
+            additionalTasks = listOf(HttpJsonGenerator(mapOf(
+                    "devUrl" to "http://dev.workspace.zran.co.nz/api/v1",
+                    "devUsername" to "admin",
+                    "devPassword" to "1qazxsw2",
+                    "testUrl" to "https://test.zran.co.nz//api/v1",
+                    "testUsername" to "admin",
+                    "testPassword" to "1qazxsw2"
+            )))
+    )
 }
