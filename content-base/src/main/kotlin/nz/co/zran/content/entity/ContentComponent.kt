e5a65dafd55936a7209575b764a3870711975ef0
Leon Zhou <zhouleib1412@gmail.com>
2020-11-30 11:23:59 +1300

    0.1.8

--- content-base/src/main/kotlin/nz/co/zran/content/entity/ContentComponent.kt
+++ content-base/src/main/kotlin/nz/co/zran/content/entity/ContentComponent.kt
@@ -2,11 +2,8 @@ package nz.co.zran.content.entity
 
 import com.github.b1412.api.entity.BaseEntity
 import com.github.b1412.generator.metadata.EntityFeature
-import com.github.b1412.permission.graphql.annotation.GraphQLIgnore
-import nz.co.zran.content.entity.json.ExternalConfig
 import org.hibernate.annotations.DynamicInsert
 import org.hibernate.annotations.DynamicUpdate
-import org.hibernate.annotations.Type
 import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
@@ -23,11 +20,12 @@ data class ContentComponent(
         @OneToMany(orphanRemoval = true, cascade = [CascadeType.ALL])
         val components: MutableList<BaseComponent> = mutableListOf(),
 
-        @Type(type = "json")
-        @Column(columnDefinition = "json")
-        @GraphQLIgnore
-        val externalConfig: List<ExternalConfig> = mutableListOf(),
-        val externalConfigString: String? = null,
+//        @Type(type = "json")
+//        @Column(columnDefinition = "json")
+//        @GraphQLIgnore
+//        val externalConfigJson: List<ExternalConfig> = mutableListOf(),
+        @Column(columnDefinition = "mediumtext")
+        val externalConfig: String? = null,
         val externalName: String? = null,
         val externalId: String? = null
 ) : BaseEntity()
